fc3a985979326c7ee1c463cfa896f4075a252c60
José Magalhães <joses.magalhaes@hotmail.com>
2023-08-18 14:36:51 -0300

    Declarando variáveis importantes

---
+++ Assets/Scripts/CascoVerde.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CascoVerde : MonoBehaviour
+{
+    private Rigidbody2D oRigidbody2D;
+    private float velocidadeHorizontal;
+
+
+    private void Awake()
+    {
+        oRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
